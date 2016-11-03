9c5f95fbfcf47086819480eec3afbd0794e07ab8
利开团 <likaituan@wecash.net>
2016-11-03 18:31:28 +0800

    first commit

---
+++ nobox.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    static: {
+        items:[{
+            dir: __dirname,
+            path: "/"
+        }]
+    },
+    pub:{
+      packages: ["index.html"]
+    },
+    port: 2000
+};
